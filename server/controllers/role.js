0db77c7d2285316bf49d2ea2f802ba9e5f4b4a2e
carloxmof1 <carlosmof15@gmail.com>
2019-09-13 21:41:44 -0300

    correcion roles

--- server/controllers/role.js
+++ server/controllers/role.js
@@ -13,48 +13,54 @@ function clean(obj) {
 class Role {
 
     static List(req, res) {
+        var cod_usuario= req.user.id;
         var role= req.body.role;
-        return Usuario.findAll({
-            where:{role:role},
-            attributes: ['id','rut','nombre'],
-            include:[
-                {
-                  model:Carrera,
-                  attributes: [['id','value'],['nombre','label']],
-                }
-            ]     
-        
+        return Usuario.findByPk(cod_usuario).then(user=>{
+
+            return Usuario.findAll({
+                where:{role:role, cod_carrera:user.cod_carrera},
+                attributes: ['id','rut','nombre'],
+                include:[
+                    {
+                      model:Carrera,
+                      attributes: [['id','value'],['nombre','label']],
+                    }
+                ]     
+            
+            })
+            .then(usuario=>res.status(200).send(usuario));
         })
-        .then(usuario=>res.status(200).send(usuario));
+   
         
     }
     
 
     static Edit(req, res) {
         var {rut,role,carrera} = req.body;
+        var cod_usuario = req.user.id;
 
         var object={
             role:role,
             cod_carrera:carrera,
         }
         object= clean(object)
-        console.log(object)
-        Usuario.findOne({where:{rut:rut}}).then(u=>{
-            if(u){
-            return  Usuario.update(object,{where:{rut:rut}})
-                .then(user=> {
-                    return res.status(200).send({status: true,data:user, message:"Rol editado"})
-                })
-                .catch(err =>{
-                    return res.status(200).send({status: false,data:err, message:"No se pudo completar la acción"})
-                })
-            }
-            else{
-                return res.status(200).send({status: false, message:"El rut ingresado no es valido"})
-              
-            }
-        })
-
+        return Usuario.findByPk(cod_usuario).then(us=>{
+            Usuario.findOne({where:{rut:rut,cod_carrera:us.cod_carrera}}).then(u=>{
+                if(u){
+                return  Usuario.update(object,{where:{rut:rut}})
+                    .then(user=> {
+                        return res.status(200).send({status: true,data:user, message:"Rol editado"})
+                    })
+                    .catch(err =>{
+                        return res.status(200).send({status: false,data:err, message:"No se pudo completar la acción"})
+                    })
+                }
+                else{
+                    return res.status(200).send({status: false, message:"No se ha encontrado el usuario"})
+                
+                }
+            })
+        });
                 
          
 
